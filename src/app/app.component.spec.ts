435d1e816192ecaae92953648b45d90d702d470e
Rooarii <rooarii.manuel@gmail.com>
2021-03-08 23:07:19 +0100

    Add ESLint, Husky
    Refactor App

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,35 +1,36 @@
-import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
+  let fixture: ComponentFixture<AppComponent>;
+  let de: DebugElement;
+  let app: AppComponent;
+  let h1: DebugElement;
+
+  beforeEach(() => {
+    fixture = TestBed.configureTestingModule({
+      declarations: [AppComponent]
+    }).createComponent(AppComponent);
+    fixture.detectChanges();
+    de = fixture.debugElement;
+    app = fixture.componentInstance;
+    h1 = de.query(By.css('h1'));
+    return { de, app, h1 };
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'pokeApp'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('pokeApp');
+  it(`should have as title 'Pokemon'`, () => {
+    expect(app.title).toEqual('Pokemon');
   });
 
   it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('pokeApp app is running!');
+    expect((h1.nativeElement as HTMLElement).innerText).toBe(
+      `${app.title} is running!`
+    );
   });
 });
